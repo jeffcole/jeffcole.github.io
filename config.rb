705bff4da664643a6b5364862983ece2c213bcab
Jeff Cole <jeffreydonaldcole@gmail.com>
2016-03-12 13:44:20 -0700

    Deploy to the master branch

--- config.rb
+++ config.rb
@@ -23,6 +23,8 @@ end
 activate :deploy do |deploy|
   deploy.build_before = true
   deploy.deploy_method = :git
+  deploy.branch = "master"
+  deploy.commit_message = ":shipit:"
 end
 
 activate :blog do |blog|
