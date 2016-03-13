ce61902e54949b5bdfe3fd286d8529b6317a2ae7
Jeff Cole <jeffreydonaldcole@gmail.com>
2016-03-12 18:26:59 -0700

    Add blog layout

--- config.rb
+++ config.rb
@@ -6,7 +6,7 @@ set :css_dir, "assets/stylesheets"
 set :js_dir, "assets/javascripts"
 set :images_dir, "assets/images"
 set :fonts_dir, "assets/fonts"
-set :layout, "layouts/application"
+set :layout, "application"
 
 page "/*.xml", layout: false
 page "/*.json", layout: false
@@ -30,23 +30,10 @@ end
 activate :blog do |blog|
   # This will add a prefix to all links, template references and source paths
   blog.prefix = "blog"
+
   # Matcher for blog source files
   blog.sources = "posts/{year}-{month}-{day}-{title}.html"
-  blog.layout = "layouts/application"
-  blog.tag_template = "/blog/tag.html"
-  blog.calendar_template = "blog/calendar.html"
 
+  blog.layout = "blog"
   blog.permalink = "{title}.html"
-  # blog.taglink = "tags/{tag}.html"
-  # blog.summary_separator = /(READMORE)/
-  # blog.summary_length = 250
-  # blog.year_link = "{year}.html"
-  # blog.month_link = "{year}/{month}.html"
-  # blog.day_link = "{year}/{month}/{day}.html"
-  # blog.default_extension = ".md"
-
-  # Enable pagination
-  # blog.paginate = true
-  # blog.per_page = 10
-  # blog.page_link = "page/{num}"
 end
