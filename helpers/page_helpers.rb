74257c2917d84035f0f392019e563ecb166dfa01
Jeff Cole <jeffreydonaldcole@gmail.com>
2016-03-15 07:14:50 -0600

    Add `page_title` helper

--- helpers/page_helpers.rb
+++ helpers/page_helpers.rb
@@ -2,4 +2,14 @@ module PageHelpers
   def absolute_url
     config.site.host + current_page.url
   end
+
+  def page_title
+    default_title = data.defaults.page_title
+
+    if current_page.try(:title).present?
+      "#{default_title} - #{current_page.title}"
+    else
+      default_title
+    end
+  end
 end
