cc87f87115006c879c6ebc999fefb1855fd9ae5a
Jeff Cole <jeffreydonaldcole@gmail.com>
2016-03-19 12:42:02 -0600

    Extract lib/twitter_url.rb

--- helpers/social_helpers.rb
+++ helpers/social_helpers.rb
@@ -1,3 +1,5 @@
+require "lib/twitter_url"
+
 module SocialHelpers
   def open_graph_type
     root? ? "profile" : "article"
@@ -9,34 +11,6 @@ module SocialHelpers
     link_to url, target: "_blank" do
       image_tag("/assets/images/icons/twitter.svg") +
         content_tag(:span) { "Tweet this" }
-    end
-  end
-
-  private
-
-  class TwitterUrl
-    attr_private_initialize :title, :absolute_url
-
-    def to_s
-      [base_url, query].join
-    end
-
-    private
-
-    def base_url
-      "http://twitter.com/home?"
-    end
-
-    def query
-      { status: status }.to_query
-    end
-
-    def status
-      "Read '#{title}' #{handle} #{absolute_url}"
-    end
-
-    def handle
-      "@obscurehobo"
     end
   end
 end
