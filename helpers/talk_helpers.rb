9934ff03263d459f45fb9d59e11efb98cb57a6e6
Jeff Cole <jeffreydonaldcole@gmail.com>
2017-02-18 13:29:45 -0700

    Add talk event and venue links

--- helpers/talk_helpers.rb
+++ helpers/talk_helpers.rb
@@ -4,4 +4,28 @@ module TalkHelpers
   def talks
     Talk.all(data.talks)
   end
+
+  def talk_event(occurence)
+    if occurence.event_url
+      link_to occurence.event_name, occurence.event_url
+    else
+      occurence.event_name
+    end
+  end
+
+  def talk_venue(occurence)
+    if occurence.venue_url
+      link_to occurence.venue_name, occurence.venue_url
+    else
+      occurence.venue_name
+    end
+  end
+
+  def talk_slides(occurence)
+    link_to "Slides", occurence.slides_url
+  end
+
+  def talk_video(occurence)
+    link_to "Video", occurence.video_url
+  end
 end
--- lib/talk.rb
+++ lib/talk.rb
@@ -11,18 +11,32 @@ class Talk
   end
 
   class Occurrence
-    attr_reader_initialize :date, :event, :venue, :slides_url, :video_url
+    attr_reader_initialize(
+      :date,
+      :event_name,
+      :event_url,
+      :venue_name,
+      :venue_url,
+      :slides_url,
+      :video_url
+    )
 
     def self.from_hash_list(list)
       list.map do |attributes|
         new(
           Date.parse(attributes[:date]),
-          attributes[:event],
-          attributes[:venue],
+          attributes[:event_name],
+          attributes[:event_url],
+          attributes[:venue_name],
+          attributes[:venue_url],
           attributes[:slides_url],
           attributes[:video_url]
         )
       end
+    end
+
+    def video?
+      video_url
     end
   end
 end
